53205a3076a60f21c023aac76aa3c01aa791c6d4
Akishika Issui <kshkss@users.noreply.github.com>
2020-02-17 03:21:54 +0900

    Modify scripts
    - mean is renamed to z-project and added new functions
    - add rotation-center
    - factoring recon; normalization ofr projection data is defined in
    util.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     package_dir={'': 'src'},
     include_package_data = True,
     package_data = {'': ['libct.so']},
-    scripts=['scripts/mean', 'scripts/recon'],
+    scripts=['scripts/z-project', 'scripts/recon', 'scripts/rotation-center'],
     python_requires='>=3.6, <4',
-    install_requires=['numpy>=1.17'],
+    install_requires=['numpy>=1.17', 'scipy>=1.1.0', 'tifffile>=2019.7.26', 'tqdm'],
     )
 
